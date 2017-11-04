a7ba654581a07f03f48d03d25cdf7653d3e6fbd5
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-11-04 18:18:56 -0400

    refactor: remove website-related content from webapp
    for some time we've been representing content on both the static site and the the webapp, this removes all website related stuff from the webapp

--- src/js/components/item/UrlItem.js
+++ src/js/components/item/UrlItem.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React from 'react'
+import { Link } from 'react-router'
 
-import { concatUrlString, containsContent } from '../../selectors/url';
+import { concatUrlString, containsContent } from '../../selectors/url'
 
 const UrlItem = ({ data }) => {
-  const url = data;
-  const uri = encodeURIComponent(url.url);
+  const url = data
+  const uri = encodeURIComponent(url.url)
 
   const urlState = (state) => {
     if (state) {
-      return <p>{url.state.loading ? "loading" : ""} {url.state.success ? "success!" : ""} {url.state.error ? url.state.error : "" }</p>;
+      return <p>{url.state.loading ? 'loading' : ''} {url.state.success ? 'success!' : ''} {url.state.error ? url.state.error : '' }</p>
     }
-    return undefined;
-  };
+    return undefined
+  }
 
   return (
-    <div className="url item">
-      <Link className="title" to={`/url?url=${uri}`}>
+    <div className='url item col-md-12'>
+      <Link className='title' to={`/url?url=${uri}`}>
         { url.title ? <h5>{url.title}</h5> : undefined }
         {concatUrlString(url.url)}
       </Link>
-      <p>{containsContent(url) && <Link className="content" to={`/content/${url.hash}`}>{url.fileName || "unnamed content"}</Link>}</p>
+      <p>{containsContent(url) && <Link className='content' to={`/content/${url.hash}`}>{url.fileName || 'unnamed content'}</Link>}</p>
       {urlState(url.state)}
     </div>
-  );
-};
+  )
+}
 
 UrlItem.propTypes = {
-  data: React.PropTypes.object.isRequired,
+  data: React.PropTypes.object.isRequired
   // onSelect: React.PropTypes.func.isRequired,
-};
+}
 
 UrlItem.defaultProps = {
-};
+}
 
-export default UrlItem;
+export default UrlItem
--- src/js/selectors/collections.js
+++ src/js/selectors/collections.js
@@ -1,28 +1,33 @@
 
-export function selectLocalCollection(state, id) {
-  return state.locals.collections[id];
+export function selectLocalCollection (state, id) {
+  return state.locals.collections[id]
 }
 
-export function selectCollection(state, id = "") {
-  return state.entities.collections[id];
+export function selectCollection (state, id = '') {
+  return state.entities.collections[id]
 }
 
-export function selectCollections(state) {
-  const { collections } = state.entities;
-  return Object.keys(collections).map(k => collections[k]);
+export function selectCollections (state) {
+  const { collections } = state.entities
+  return Object.keys(collections).map(k => collections[k])
 }
 
-export function selectCollectionItems(state, id) {
-  const { collectionItems } = state.entities;
-  const ci = state.pagination.collectionItems[id];
+export function selectRecentCollections (state, count = 3) {
+  const { collections } = state.entities
+  return Object.keys(collections).map(k => collections[k]).slice(0, count)
+}
+
+export function selectCollectionItems (state, id) {
+  const { collectionItems } = state.entities
+  const ci = state.pagination.collectionItems[id]
   if (ci) {
-    return ci.ids.map(id => collectionItems[id]);
+    return ci.ids.map(id => collectionItems[id])
   }
 
-  return [];
+  return []
 }
 
-export function selectCollectionsByKey(state, key) {
-  const { collections } = state.entities;
-  return Object.keys(collections).filter(id => collections[id].creator == key).map(id => collections[id]);
+export function selectCollectionsByKey (state, key) {
+  const { collections } = state.entities
+  return Object.keys(collections).filter(id => collections[id].creator == key).map(id => collections[id])
 }
